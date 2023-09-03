274919589bc98eb1dd669600485ffe38bc325b36
vamsigoli <vamsi.goli@gmail.com>
2023-09-03 11:26:25 -0400

    Create working-file-for-the-tabitosan-method.sql

---
+++ sql/analytical-sql-examples/working-file-for-the-tabitosan-method.sql
@@ -0,0 +1,13 @@
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-01');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-02');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-03');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-04');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-07');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-08');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-09');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-10');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-14');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-15');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-16');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-19');
+insert into LAB_SAMPLES ( date_taken) values (date '2015-12-20');
